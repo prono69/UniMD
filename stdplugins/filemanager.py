21ff6d8e792b2b8a07f8e836a0aa8edfccbd8be7
M.Furkan <byazade@yandex.com>
2019-03-28 20:33:08 +0300

    Create filemanager.py

---
+++ stdplugins/filemanager.py
@@ -0,0 +1,87 @@
+"""Execute GNU/Linux commands inside Telegram
+Syntax: .exec Code"""
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from telethon import events
+import subprocess
+from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
+import io
+import asyncio
+import time
+
+@borg.on(events.NewMessage(pattern=r"\.lslocal", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+#    dirname = event.pattern_match.group(1)
+#    tempdir = "localdir"
+    cmd = "ls ./DOWNLOADS/"
+#    if dirname == tempdir:
+	
+    eply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    start_time = time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    OUTPUT = f"**Files in DOWNLOADS folder:**\n"
+    stdout, stderr = await process.communicate()
+    if len(stdout) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(stdout)) as out_file:
+            out_file.name = "exec.text"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=OUTPUT,
+                reply_to=reply_to_id
+            )
+            await event.delete()
+    if stderr.decode():
+        await event.edit(f"**{stderr.decode()}**")
+        return
+    await event.edit(f"{OUTPUT}`{stdout.decode()}`")
+#    else:
+#        await event.edit("Unknown Command")
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.lsroot", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+    cmd = "ls"
+	
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    start_time = time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    OUTPUT = f"**Files in root directory:**\n"
+    stdout, stderr = await process.communicate()
+    if len(stdout) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(stdout)) as out_file:
+            out_file.name = "exec.text"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=OUTPUT,
+                reply_to=reply_to_id
+            )
+            await event.delete()
+    if stderr.decode():
+        await event.edit(f"**{stderr.decode()}**")
+        return
+    await event.edit(f"{OUTPUT}`{stdout.decode()}`")
+
