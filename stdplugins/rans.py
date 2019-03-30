cbd5ccf6eadc5ac27ad26c3e5904664f3ec5e873
M.Furkan <byazade@yandex.com>
2019-03-30 14:04:36 +0300

    new plugin

---
+++ stdplugins/rans.py
@@ -0,0 +1,18 @@
+﻿# (c) @UniBorg
+# Original written by @UniBorg edit by @INF1N17Y
+
+from telethon import events
+import asyncio
+from collections import deque
+
+
+@borg.on(events.NewMessage(pattern=r"\.kos", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🚶🏃🚶🏃🚶🏃🚶🏃"))
+	for _ in range(48):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+    
