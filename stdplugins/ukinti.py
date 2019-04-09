a3d641b1a6ac6c1fb3e8d0a95df41aa58d7cade6
M.Furkan <byazade@yandex.com>
2019-04-09 22:52:23 +0300

    Update ukinti.py

--- stdplugins/ukinti.py
+++ stdplugins/ukinti.py
@@ -3,12 +3,17 @@ Available Commands:
 .unbanall
 .kick option
 Available Options: d, y, m, w, o, q, r """
-from telethon import events
-from datetime import datetime, timedelta
-from telethon.tl.types import UserStatusEmpty, UserStatusLastMonth, UserStatusLastWeek, UserStatusOffline, UserStatusOnline, UserStatusRecently, ChannelParticipantsKicked, ChatBannedRights
-from telethon.tl import functions, types
-from time import sleep
 import asyncio
+from datetime import datetime, timedelta
+from time import sleep
+
+from telethon import events
+from telethon.tl import functions, types
+from telethon.tl.types import (ChannelParticipantsKicked, ChatBannedRights,
+                               UserAdminInvalidError, UserNotParticipantError,
+                               UserStatusEmpty, UserStatusLastMonth,
+                               UserStatusLastWeek, UserStatusOffline,
+                               UserStatusOnline, UserStatusRecently, FloodWaitError)
 
 
 @borg.on(events.NewMessage(pattern="\.unbanall ?(.*)", outgoing=True))
