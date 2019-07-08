8d05340d0908e1d03e198f1b4d0912955b3f0fc8
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-07-08 21:09:03 +0530

    ad5c3f2d18b827790e5b201725b2b10c77666ab9 27

--- sample_config.py
+++ sample_config.py
@@ -99,6 +99,7 @@ class Config(object):
     # Google Drive ()
     G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
     G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
+    G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
 
 
 class Production(Config):
