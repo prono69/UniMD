c884e51f60519e219b953b18e5bb0583961a6f0d
M.Furkan <byazade@yandex.com>
2019-07-18 22:30:27 +0300

    Update pmpermit.py

--- stdplugins/pmpermit.py
+++ stdplugins/pmpermit.py
@@ -17,8 +17,8 @@ borg.storage.PREV_REPLY_MESSAGE = {}
 
 BAALAJI_TG_USER_BOT = "My Master hasn't approved you to PM."
 TG_COMPANION_USER_BOT = "Please wait for his response and don't spam his PM."
-UNIBORG_USER_BOT_WARN_ZERO = "I am currently offline. Please do not SPAM me."
-UNIBORG_USER_BOT_NO_WARN = "Hi! I will answer to your message soon. Please wait for my response and don't spam my PM. Thanks"
+UNIBORG_USER_BOT_WARN_ZERO = "Şuan burada değilim. Lütfen ben gelene kadar bekleyin."
+UNIBORG_USER_BOT_NO_WARN = "Merhaba, mesajını yakında cevaplayacağım.. Lütfen birden çok mesaj atmadan önce bekle yakında burada olacağım. Anlayışın için teşekkürler"
 
 
 @borg.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
