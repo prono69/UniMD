8a808965b66395ae3e2e29fc692f7f6a13437fb2
M.Furkan <byazade@yandex.com>
2019-07-24 22:58:34 +0300

    Update afk.py

--- stdplugins/afk.py
+++ stdplugins/afk.py
@@ -100,10 +100,11 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"`I'm currently away from keyboard since` {afk_since} " + \
-            f"`And I will be right back soon`.\n__Reason:__ {reason}" \
-            if reason \
-            else f"I'm currently away from keyboard since {afk_since}. Meanwhile, you can spam admins here with Noods in PM.(◕‿◕)  Until I come back to check your USELESS messages. "
+        message_to_reply = "Şuan burada değilim. Yakında mesajına bakacağım. \n **Sebebi: {reason}"
+        #f"`I'm currently away from keyboard since` {afk_since} " + \
+            #f"`And I will be right back soon`.\n__Reason:__ {reason}" \
+            #if reason \
+            #else f"I'm currently away from keyboard since {afk_since}. Meanwhile, you can spam admins here with Noods in PM.(◕‿◕)  Until I come back to check your USELESS messages. "
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
         if event.chat_id in borg.storage.last_afk_message:  # pylint:disable=E0602
