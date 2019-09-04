85700b9790b9c9d16cf2a98974293e1a6910700d
M.Furkan <byazade@yandex.com>
2019-09-04 19:11:26 +0300

    Update openweathermap.py

--- stdplugins/openweathermap.py
+++ stdplugins/openweathermap.py
@@ -2,6 +2,7 @@
 Syntax: .weather <Location> """
 
 import aiohttp
+import io
 import time
 from datetime import tzinfo, datetime
 from uniborg.util import admin_cmd
@@ -22,14 +23,13 @@ async def _(event):
         sun_rise_time = int(response_api["sys"]["sunrise"]) + country_time_zone
         sun_set_time = int(response_api["sys"]["sunset"]) + country_time_zone
         await event.edit(
-            """**{}**
-            
+            """{}
 **Temperature**: {}°С
-**Min**: {}°С
-**Max**: {}°С
+    __minimium__: {}°С
+    __maximum__ : {}°С
 **Humidity**: {}%
-**Wind**: {}m/s
-**Clouds**: {}hpa
+**wind**: {}m/s
+clouds: {}hpa
 **Sunrise**: {} {}
 **Sunset**: {} {}""".format(
                 input_str,
@@ -48,3 +48,21 @@ async def _(event):
         )
     else:
         await event.edit(response_api["message"])
+
+
+@borg.on(admin_cmd(pattern="wttr (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    sample_url = "https://wttr.in/{}.png"
+    # logger.info(sample_url)
+    input_str = event.pattern_match.group(1)
+    async with aiohttp.ClientSession() as session:
+        response_api_zero = await session.get(sample_url.format(input_str))
+        # logger.info(response_api_zero)
+        response_api = await response_api_zero.read()
+        with io.BytesIO(response_api) as out_file:
+            await event.reply(
+                file=out_file
+            )
+    await event.edit(input_str)
