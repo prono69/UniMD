9896f7c36d4b688447adb69ba794e64d313f9be7
M.Furkan <byazade@yandex.com>
2019-09-09 16:39:25 +0300

    Update log_pms.py

--- stdplugins/log_pms.py
+++ stdplugins/log_pms.py
@@ -14,7 +14,7 @@ async def monito_p_m_s(event):
     sender = await event.get_sender()
     if Config.NC_LOG_P_M_S and not sender.bot:
         chat = await event.get_chat()
-        if chat.id not in borg.storage.NO_PM_LOG_USERS and chat.id != borg.uid:
+        if chat.id not in NO_PM_LOG_USERS and chat.id != borg.uid:
             try:
                 e = await borg.get_entity(int(Config.PM_LOGGR_BOT_API_ID))
                 fwd_message = await borg.forward_messages(
@@ -34,8 +34,8 @@ async def approve_p_m(event):
     chat = await event.get_chat()
     if Config.NC_LOG_P_M_S:
         if event.is_private:
-            if chat.id not in borg.storage.NO_PM_LOG_USERS:
-                borg.storage.NO_PM_LOG_USERS.append(chat.id)
+            if chat.id not in NO_PM_LOG_USERS:
+                NO_PM_LOG_USERS.append(chat.id)
                 await event.edit("Won't Log Messages from this chat")
                 await asyncio.sleep(3)
                 await event.delete()
