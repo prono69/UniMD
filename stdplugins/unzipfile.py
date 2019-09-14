3476bdc5437c13c7e96fa6c8236ce5064a7f67ad
M.Furkan <byazade@yandex.com>
2019-09-14 23:37:02 +0300

    Update unzipfile.py

--- stdplugins/unzipfile.py
+++ stdplugins/unzipfile.py
@@ -1,5 +1,6 @@
 """ command: .unzip
 coded by @By_Azade
+code rewritten my SnapDragon7410
 """
 
 import asyncio
@@ -13,12 +14,16 @@ from uniborg.util import admin_cmd, humanbytes, progress, time_formatter
 import time
 from datetime import datetime
 from pySmartDL import SmartDL
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
 from zipfile import ZipFile
 
 extracted = Config.TMP_DOWNLOAD_DIRECTORY + "extracted/"
+if not os.path.isdir(extracted):
+    os.makedirs(extracted)
 
 
-@borg.on(admin_cmd(pattern=("unzip")))
+@borg.on(admin_cmd(pattern="unzip"))
 async def _(event):
     if event.fwd_from:
         return
@@ -42,39 +47,79 @@ async def _(event):
         else:
             end = datetime.now()
             ms = (end - start).seconds
-            await mone.edit("Downloaded to `{}` in {} seconds.".format(downloaded_file_name, ms))
+            await mone.edit("Stored the zip to `{}` in {} seconds.".format(downloaded_file_name, ms))
 
-        unzip = zipfile.ZipFile(downloaded_file_name,'r')
-        unzip.extractall(path=extracted)
-        filename = downloaded_file_name
-        filename = os.path.basename(filename)
-        filename = os.path.splitext(filename)[0]
-        unzipped = extracted + filename
-        files = []
-        pathh = extracted
+        with zipfile.ZipFile(downloaded_file_name, 'r') as zip_ref:
+            zip_ref.extractall(extracted)
+        filename = sorted(get_lst_of_files(extracted, []))
+        #filename = filename + "/"
         await event.edit("Unzipping now")
         # r=root, d=directories, f = files
-        for r, d, f in os.walk(pathh):
-            for file in f:
-                files.append(os.path.join(r, file))
-                print(files)
+        for single_file in filename:
+            if os.path.exists(single_file):
+                # https://stackoverflow.com/a/678242/4723940
+                caption_rts = os.path.basename(single_file)
+                force_document = True
+                supports_streaming = False
+                document_attributes = []
+                if single_file.endswith((".mp4", ".mp3", ".flac", ".webm")):
+                    metadata = extractMetadata(createParser(single_file))
+                    duration = 0
+                    width = 0
+                    height = 0
+                    if metadata.has("duration"):
+                        duration = metadata.get('duration').seconds
+                    if os.path.exists(thumb_image_path):
+                        metadata = extractMetadata(createParser(thumb_image_path))
+                        if metadata.has("width"):
+                            width = metadata.get("width")
+                        if metadata.has("height"):
+                            height = metadata.get("height")
+                    document_attributes = [
+                        DocumentAttributeVideo(
+                            duration=duration,
+                            w=width,
+                            h=height,
+                            round_message=False,
+                            supports_streaming=True
+                        )
+                    ]
+                try:
+                    await borg.send_file(
+                        event.chat_id,
+                        single_file,
+                        caption=f"UnZipped `{caption_rts}`",
+                        force_document=force_document,
+                        supports_streaming=supports_streaming,
+                        allow_cache=False,
+                        reply_to=event.message.id,
+                        attributes=document_attributes,
+                        # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                        #     progress(d, t, event, c_time, "trying to upload")
+                        # )
+                    )
+                except Exception as e:
+                    await borg.send_message(
+                        event.chat_id,
+                        "{} caused `{}`".format(caption_rts, str(e)),
+                        reply_to=event.message.id
+                    )
+                    # some media were having some issues
+                    continue
+                os.remove(single_file)
+        os.remove(downloaded_file_name)
 
-        for f in files:
-            await borg.send_file(
-                event.chat_id,
-                f,
-                caption="unzipped @By_Azade",
-                force_document=True,
-                supports_streaming=True,
-                allow_cache=True                            
-            )
-            
-        print(files)
-        print("-----")
-        print(unzipped)
-        print("-----")
-        print(filename)
-        print("-----")
-        print(downloaded_file_name)
 
-            
+
+
+
+
+
+def get_lst_of_files(input_directory, output_lst):
+    filesinfolder = os.listdir(input_directory)
+    for file_name in filesinfolder:
+        current_file_name = os.path.join(input_directory, file_name)
+        if os.path.isdir(current_file_name):
+            return get_lst_of_files(current_file_name, output_lst)
+        output_lst.append(current_file_name)
+    return output_lst
