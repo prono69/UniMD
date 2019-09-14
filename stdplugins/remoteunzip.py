b37281159133256f1666f5347cc4fb5968efbdbf
M.Furkan <byazade@yandex.com>
2019-09-14 18:22:27 +0300

    Update remoteunzip.py

--- stdplugins/remoteunzip.py
+++ stdplugins/remoteunzip.py
@@ -36,7 +36,7 @@ async def _(event):
     else:
         with RemoteZip(links) as zip:
             zip.extractall(filedir)
-            x = zip_ref.extractall(filedir)
+            x = zip.extractall(filedir)
             await borg.send_file(
                             event.chat_id,
                             x,
