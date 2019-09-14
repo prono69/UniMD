c602934a3aed8fc7c969203a7a424e02c86ad75b
M.Furkan <byazade@yandex.com>
2019-09-14 21:19:51 +0300

    Update unzipfile.py

--- stdplugins/unzipfile.py
+++ stdplugins/unzipfile.py
@@ -67,7 +67,7 @@ async def _(event):
             await borg.send_file(
                             event.chat_id,
                             f,
-                            caption="unzipped @By_Azade",
+                            caption="unzipped @By_Azade"
                             # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
                             #     progress(d, t, event, c_time, "trying to upload")
                             # )
