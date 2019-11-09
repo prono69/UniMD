9c3c1288c174f8241f4beb2405b56c7ea8ceec0b
M.Furkan <byazade@yandex.com>
2019-11-09 20:50:41 +0300

    update

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -44,7 +44,7 @@ async def _(event):
 
     me = borg.me
     userid = event.from_id
-    packname = f"@By_Azqde Pack"
+    packname = f"@By_Azade Pack"
     packshortname = f"Uni_Borg_{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
