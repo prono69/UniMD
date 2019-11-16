37b6f0b426f8536dc1710d5c73fa1d1c9416f21c
M.Furkan <byazade@yandex.com>
2019-11-16 20:28:08 +0300

    .

--- stdplugins/tarfile.py
+++ stdplugins/tarfile.py
@@ -1,14 +1,14 @@
 import asyncio
 import os
-import time
+import shutil
 import tarfile
+import time
 
 from pySmartDL import SmartDL
+from sample_config import Config
 from telethon import events
-
 from uniborg.util import admin_cmd, humanbytes, progress, time_formatter
 
-from sample_config import Config
 
 @borg.on(admin_cmd(pattern=("tar ?(.*)")))
 async def _(event):
@@ -32,18 +32,26 @@ async def _(event):
             directory_name = downloaded_file_name
             await event.edit("Finish downloading to my local")
             # zipfile.ZipFile(directory_name + '.zip', 'w', zipfile.ZIP_DEFLATED).write(directory_name)
-            out_tar = tardir(directory_name,directory_name )
+            # out_tar = tardir(directory_name,directory_name )
+            to_upload_file = directory_name 
+            if is_zip:
+                # first check if current free space allows this
+                # ref: https://github.com/out386/aria-telegram-mirror-bot/blob/master/src/download_tools/aria-tools.ts#L194
+                # archive the contents
+                check_if_file = await create_archive(to_upload_file)
+                if check_if_file is not None:
+                    to_upload_file = check_if_file
             await borg.send_file(
                 event.chat_id,
-                out_tar + ".tar.gz",
+                to_upload_file,
                 caption="TAR By @By_Azade",
                 force_document=True,
                 allow_cache=False,
                 reply_to=event.message.id,
             )
             try:
-                os.remove(out_tar + ".tar.gz")
-                os.remove(out_tar)
+                os.remove(to_upload_file)
+                os.remove(to_upload_file)
             except:
                     pass
             await event.edit("Task Completed")
@@ -53,16 +61,52 @@ async def _(event):
             await mone.edit(str(e))
     elif input_str:
         directory_name = input_str
-        out_tar = tardir(directory_name,directory_name )
-        await event.edit("Local file compressed to `{}`".format(directory_name + ".tar.gz"))
+        
+        await event.edit("Local file compressed to `{}`".format(to_upload_file))
 
         
-def make_tarfile(output_filename, source_dir):
-    with tarfile.open(output_filename, "w:gz") as tar:
-        tar.add(source_dir, arcname=os.path.basename(source_dir))
+# def make_tarfile(output_filename, source_dir):
+#     with tarfile.open(output_filename, "w:gz") as tar:
+#         tar.add(source_dir, arcname=os.path.basename(source_dir))
 
-def tardir(path, tar_name):
-    with tarfile.open(tar_name, "w:gz") as tar_handle:
-        for root, dirs, files in os.walk(path):
-            for file in files:
-                tar_handle.add(os.path.join(root, file))
+# def tardir(path, tar_name):
+#     with tarfile.open(tar_name, "w:gz") as tar_handle:
+#         for root, dirs, files in os.walk(path):
+#             for file in files:
+#                 tar_handle.add(os.path.join(root, file))
+
+
+async def create_archive(input_directory):
+    return_name = None
+    if os.path.exists(input_directory):
+        base_dir_name = os.path.basename(input_directory)
+        compressed_file_name = f"{base_dir_name}.tar.gz"
+        # #BlameTelegram
+        suffix_extention_length = 1 + 3 + 1 + 2
+        if len(base_dir_name) > (64 - suffix_extention_length):
+            compressed_file_name = base_dir_name[0:(64 - suffix_extention_length)]
+            compressed_file_name += ".tar.gz"
+        # fix for https://t.me/c/1434259219/13344
+        file_genertor_command = [
+            "tar",
+            "-zcvf",
+            compressed_file_name,
+            f"{input_directory}"
+        ]
+        process = await asyncio.create_subprocess_exec(
+            *file_genertor_command,
+            # stdout must a pipe to be accessible as process.stdout
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        # Wait for the subprocess to finish
+        stdout, stderr = await process.communicate()
+        e_response = stderr.decode().strip()
+        t_response = stdout.decode().strip()
+        if os.path.exists(compressed_file_name):
+            try:
+                shutil.rmtree(input_directory)
+            except:
+                pass
+            return_name = compressed_file_name
+    return return_name
