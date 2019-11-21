21d4ed363059be996765cd70f19da6219a046dcc
muhammedfurkan <byazade@yandex.com>
2019-11-21 18:49:43 +0300

    update

--- stdplugins/sangmata.py
+++ stdplugins/sangmata.py
@@ -58,10 +58,11 @@ async def _(event):
     async with borg.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=177914997))
-              await borg.forward_messages(chat, reply_message)
+              await borg.forward_messages(chat, "/id")
+            #   await borg.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("```Please unblock @sangmatainfo_bot and try again```")
+              await event.reply("```Please unblock @fakemailbot and try again```")
               return
           if response.text.startswith("send"):
              await event.edit("```can you kindly disable your forward privacy settings for good?```")
