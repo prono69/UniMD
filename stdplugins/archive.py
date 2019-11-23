b6062ab82744544c256bdc022f4585fa435d18b7
muhammedfurkan <byazade@yandex.com>
2019-11-24 00:49:20 +0300

    heroku doesnt support iso !

--- stdplugins/archive.py
+++ stdplugins/archive.py
@@ -26,52 +26,7 @@ extracted = Config.TMP_DOWNLOAD_DIRECTORY + "extracted/"
 thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
 
 
-@borg.on(admin_cmd(pattern=("iso ?(.*)")))
-async def _(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(1)
-    mone = await event.edit("Processing ...")
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        try:
-            c_time = time.time()
-            downloaded_file_name = await borg.download_media(
-                reply_message,
-                Config.TMP_DOWNLOAD_DIRECTORY,
-                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                    progress(d, t, mone, c_time, "trying to download")
-                )
-            )
-            directory_name = downloaded_file_name
-            await event.edit("creating iso archive, please wait..")
-            # patoolib.create_archive(directory_name + '.7z',directory_name)
-            patoolib.create_archive(directory_name + ".iso",(directory_name,Config.TMP_DOWNLOAD_DIRECTORY))
-            # patoolib.create_archive("/content/21.yy Avrupa (1).pdf.zip",("/content/21.yy Avrupa (1).pdf","/content/"))
-            await borg.send_file(
-                event.chat_id,
-                directory_name + ".iso",
-                caption="ISO By @furki",
-                force_document=True,
-                allow_cache=False,
-                reply_to=event.message.id,
-            )
-            try:
-                os.remove(directory_name + ".iso")
-                os.remove(directory_name)
-            except:
-                    pass
-            await event.edit("Task Completed")
-            await asyncio.sleep(3)
-            await event.delete()
-        except Exception as e:  # pylint:disable=C0103,W0703
-            await mone.edit(str(e))
-    elif input_str:
-        directory_name = input_str
-        
-        await event.edit("Local file compressed to `{}`".format(directory_name + ".iso"))
+
 
 
 @borg.on(admin_cmd(pattern=("rar ?(.*)")))
