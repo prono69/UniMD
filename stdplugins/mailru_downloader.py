7a519b2f6dc7fea17a9877636c1e29520e2fab6a
muhammedfurkan <byazade@yandex.com>
2019-11-24 18:22:32 +0300

    .

--- stdplugins/mailru_downloader.py
+++ stdplugins/mailru_downloader.py
@@ -16,7 +16,6 @@ import io
 
 
 
-
 @borg.on(admin_cmd(pattern=("cmrdl ?(.*)")))
 async def _(event):
     url = event.pattern_match.group(1)
@@ -24,53 +23,36 @@ async def _(event):
         return
     input_str = event.pattern_match.group(1)
     mone = await event.edit("Processing ...")
-    # if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-    #     os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    # if event.reply_to_msg_id:
     start = datetime.now()
     reply_message = await event.get_reply_message()
-    
-    c_time = time.time()
-    downloaded_file_name = Config.TMP_DOWNLOAD_DIRECTORY
-    await event.edit("Finish downloading to my local")
-    command_to_exec = f"./bin/cmrudl.py {url} -d ./DOWNLOADS/"
-        # sp = subprocess.Popen(command_to_exec, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    reply_to_id = event.message.id
-    PROCESS_RUN_TIME = 100
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    start_time = time.time() + PROCESS_RUN_TIME
-    process = await asyncio.create_subprocess_shell(
-    command_to_exec, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-    )
-    logger.info(command_to_exec)
-    OUTPUT = f"**Files in DOWNLOADS folder:**\n"
-    stdout, stderr = await process.communicate()
-    t_response = stdout.decode().strip()
-    if len(stdout) > Config.MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(stdout) as out_file:
-            out_file.name = "exec.txt"
+    try:
+        c_time = time.time()
+        downloaded_file_name = Config.TMP_DOWNLOAD_DIRECTORY
+        await event.edit("Finish downloading to my local")
+        command_to_exec = [
+                "./bin/cmrudl.py",
+                url,
+                "-d",
+                "./DOWNLOADS/"
+                ]
+        process = await asyncio.create_subprocess_shell(
+        command_to_exec, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+        )
+        OUTPUT = f"**Files in DOWNLOADS folder:**\n"
+        stdout, stderr = await process.communicate()
+        if len(stdout) > Config.MAX_MESSAGE_SIZE_LIMIT:
+            file = open("exec.text","w")
+            file.write(str(stdout))
             await borg.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=OUTPUT,
-                reply_to=reply_to_id
-            )
-            with open(str(out_file), encoding="utf-8") as file:
-                x = [l.strip() for l in file]
-            output_file_name = x[1]
-            full_file_name = "./DOWNLOADS/" + output_file_name
-            await borg.send_file(
-                event.chat_id,
-                full_file_name,
-                force_document=True,
-                allow_cache=False,
-                caption=OUTPUT,
-                reply_to=reply_to_id
-            )
-    if stderr.decode():
-        await event.edit(f"**{stderr.decode()}**")
-        return
-    await event.edit(f"{OUTPUT}`{stdout.decode()}`")
+                    event.chat_id,
+                    file,
+                    force_document=True,
+                    allow_cache=False,
+                    caption=OUTPUT,
+                    reply_to=event.reply_to_id
+           )
+           await event.delete()
+        if stderr.decode():
+            await event.edit(f"**{stderr.decode()}**")
+            return
+        await event.edit(f"{OUTPUT}{stdout.decode()}")
