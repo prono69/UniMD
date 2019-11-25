9c3761bd701af9a18cbe211b39f927891cfa3121
muhammedfurkan <byazade@yandex.com>
2019-11-25 15:48:45 +0300

    .

--- stdplugins/sangmata.py
+++ stdplugins/sangmata.py
@@ -59,7 +59,7 @@ async def _(event):
     async with borg.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=177914997))
-              await borg.forward_messages(chat,"/generate")
+              await borg.forward_messages(chat,reply_message)
               response = await response 
           except YouBlockedUserError: 
               await event.reply("```Please unblock @sangmatainfo_bot and try again```")
