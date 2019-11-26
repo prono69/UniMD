610fa06bacafae5fce0c961cbeeb5e6ffc8fc1ea
muhammedfurkan <byazade@yandex.com>
2019-11-26 11:09:12 +0300

    .

--- stdplugins/remoteunzip.py
+++ stdplugins/remoteunzip.py
@@ -19,7 +19,7 @@ filedir = f"{Config.TMP_DOWNLOAD_DIRECTORY}extracted/"
 
 @borg.on(events.NewMessage(pattern=("runzip")))
 async def _(event):
-    """ remote link to unzip archive """
+    """ remote unzip with url """
     await event.edit("`Processing...`")
     textx = await event.get_reply_message()
     message = event.pattern_match.group(1)
@@ -28,24 +28,27 @@ async def _(event):
     elif textx:
         message = textx.text
     else:
-        await event.edit("`Usage: .runzip <url>`")
+        await event.edit("`Usage: .runzip <url> with .zip extention url`")
         return
     reply = ''
-    links = re.findall(r'\.zip',textx)
+    links = re.findall(r'\bhttps?://.*\.\S+', message)
     if not links:
-        reply = "`No .zip extension link found!`"
+        reply = "`No links found!`"
         await event.edit(reply)
-    else:
-        with RemoteZip(links) as zip:
-            zip.extractall(filedir)
-            x = zip.extractall(filedir)
-            await borg.send_file(
-                            event.chat_id,
-                            x,
-                            caption="unzipped @By_Azade",
-                            # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                            #     progress(d, t, event, c_time, "trying to upload")
-                            # )
-                        )
+    for link in links:
+        if links.endswith("zip") in link:
+            with RemoteZip(links) as zip:
+                zip.extractall(filedir)
+                x = zip.extractall(filedir)
+                await borg.send_file(
+                                event.chat_id,
+                                x,
+                                caption="unzipped @By_Azade",
+                                # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                                #     progress(d, t, event, c_time, "trying to upload")
+                                # )
+                            )
+        else:
+            await event.get_reply_message("there is no .zip link")
 
     await event.edit(reply)
