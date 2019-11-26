28b7bd8ba890b611cda3846bc53556df81d99af3
muhammedfurkan <byazade@yandex.com>
2019-11-26 11:44:26 +0300

    .

--- stdplugins/youtube_playlist.py
+++ stdplugins/youtube_playlist.py
@@ -30,69 +30,68 @@ async def _(event):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
     if event.reply_to_msg_id:
         reply_message = await event.get_reply_message()
-        try:
-            c_time = time.time()
-            command_to_exec = [
-                    "./bin/youtubeplaylist.py",
-                    f"{url}",
-                    f"{output}"
+        c_time = time.time()
+        command_to_exec = [
+                "./bin/youtubeplaylist.py",
+                f"{url}",
+                f"{output}"
+                ]
+        logger.info(command_to_exec)
+        sp = subprocess.Popen(command_to_exec, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+        filename = sorted(get_lst_of_files(output, []))
+        for single_file in filename:
+            if os.path.exists(single_file):
+                # https://stackoverflow.com/a/678242/4723940
+                caption_rts = os.path.basename(single_file)
+                force_document = True
+                supports_streaming = False
+                document_attributes = []
+                if single_file.endswith((".mp4", ".mp3", ".flac", ".webm")):
+                    metadata = extractMetadata(createParser(single_file))
+                    duration = 0
+                    width = 0
+                    height = 0
+                    if metadata.has("duration"):
+                        duration = metadata.get('duration').seconds
+                    if os.path.exists(thumb_image_path):
+                        metadata = extractMetadata(createParser(thumb_image_path))
+                        if metadata.has("width"):
+                            width = metadata.get("width")
+                        if metadata.has("height"):
+                            height = metadata.get("height")
+                    document_attributes = [
+                        DocumentAttributeVideo(
+                            duration=duration,
+                            w=width,
+                            h=height,
+                            round_message=False,
+                            supports_streaming=True
+                        )
                     ]
-            logger.info(command_to_exec)
-            sp = subprocess.Popen(command_to_exec, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-            filename = sorted(get_lst_of_files(output, []))
-            for single_file in filename:
-                if os.path.exists(single_file):
-                    # https://stackoverflow.com/a/678242/4723940
-                    caption_rts = os.path.basename(single_file)
-                    force_document = True
-                    supports_streaming = False
-                    document_attributes = []
-                    if single_file.endswith((".mp4", ".mp3", ".flac", ".webm")):
-                        metadata = extractMetadata(createParser(single_file))
-                        duration = 0
-                        width = 0
-                        height = 0
-                        if metadata.has("duration"):
-                            duration = metadata.get('duration').seconds
-                        if os.path.exists(thumb_image_path):
-                            metadata = extractMetadata(createParser(thumb_image_path))
-                            if metadata.has("width"):
-                                width = metadata.get("width")
-                            if metadata.has("height"):
-                                height = metadata.get("height")
-                        document_attributes = [
-                            DocumentAttributeVideo(
-                                duration=duration,
-                                w=width,
-                                h=height,
-                                round_message=False,
-                                supports_streaming=True
-                            )
-                        ]
-                    try:
-                        await borg.send_file(
-                            event.chat_id,
-                            single_file,
-                            caption=f" `{caption_rts}`",
-                            force_document=force_document,
-                            supports_streaming=supports_streaming,
-                            allow_cache=False,
-                            reply_to=event.message.id,
-                            attributes=document_attributes,
-                            # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                            #     progress(d, t, event, c_time, "trying to upload")
-                            # )
-                        )
-                    except Exception as e:
-                        await borg.send_message(
-                            event.chat_id,
-                            "{} caused `{}`".format(caption_rts, str(e)),
-                            reply_to=event.message.id
-                        )
-                        # some media were having some issues
-                        continue
-                    os.remove(single_file)
-            os.remove(output)
+                try:
+                    await borg.send_file(
+                        event.chat_id,
+                        single_file,
+                        caption=f" `{caption_rts}`",
+                        force_document=force_document,
+                        supports_streaming=supports_streaming,
+                        allow_cache=False,
+                        reply_to=event.message.id,
+                        attributes=document_attributes,
+                        # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                        #     progress(d, t, event, c_time, "trying to upload")
+                        # )
+                    )
+                except Exception as e:
+                    await borg.send_message(
+                        event.chat_id,
+                        "{} caused `{}`".format(caption_rts, str(e)),
+                        reply_to=event.message.id
+                    )
+                    # some media were having some issues
+                    continue
+                os.remove(single_file)
+        os.remove(output)
 
 
 def get_lst_of_files(input_directory, output_lst):
