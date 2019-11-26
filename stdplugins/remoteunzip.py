e7534b9d187d1c9e62f48420668442eea866478e
muhammedfurkan <byazade@yandex.com>
2019-11-26 11:36:08 +0300

    .

--- stdplugins/remoteunzip.py
+++ stdplugins/remoteunzip.py
@@ -10,45 +10,111 @@ from pySmartDL import SmartDL
 from remotezip import RemoteZip
 from telethon import events
 from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo
-
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
 from uniborg.util import admin_cmd, humanbytes, progress, time_formatter
 
 from sample_config import Config
 
 filedir = f"{Config.TMP_DOWNLOAD_DIRECTORY}extracted/"
 
-@borg.on(events.NewMessage(pattern=("runzip")))
+@borg.on(admin_cmd(pattern=("runzip ?(.*)")))
 async def _(event):
-    """ remote unzip with url """
-    await event.edit("`Processing...`")
+    if event.fwd_from:
+        return
     textx = await event.get_reply_message()
     message = event.pattern_match.group(1)
+    input_str = event.pattern_match.group(1)
     if message:
         pass
     elif textx:
         message = textx.text
     else:
-        await event.edit("`Usage: .runzip <url> with .zip extention url`")
+        await event.edit("`Usage: .direct <url>`")
         return
+    mone = await event.edit("Processing ...")
+    if not os.path.isdir(filedir):
+        os.makedirs(filedir)
     reply = ''
-    links = re.findall(r'\bhttps?://.*\.\S+', message)
+    links = message
     if not links:
         reply = "`No links found!`"
         await event.edit(reply)
-    for link in links:
-        if link.endswith("zip") in link:
-            with RemoteZip(links) as zip:
-                zip.extractall(filedir)
-                x = zip.extractall(filedir)
-                await borg.send_file(
-                                event.chat_id,
-                                x,
-                                caption="unzipped @By_Azade",
-                                # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                                #     progress(d, t, event, c_time, "trying to upload")
-                                # )
+    if links:
+        with RemoteZip(message) as zip:
+            zip.extractall(filedir)
+            x = zip.extractall(filedir)
+            filename = sorted(get_lst_of_files(x, []))
+            for single_file in filename:
+                if os.path.exists(single_file):
+                    # https://stackoverflow.com/a/678242/4723940
+                    caption_rts = os.path.basename(single_file)
+                    force_document = True
+                    supports_streaming = False
+                    document_attributes = []
+                    if single_file.endswith((".mp4", ".mp3", ".flac", ".webm")):
+                        metadata = extractMetadata(createParser(single_file))
+                        duration = 0
+                        width = 0
+                        height = 0
+                        if metadata.has("duration"):
+                            duration = metadata.get('duration').seconds
+                        if os.path.exists(thumb_image_path):
+                            metadata = extractMetadata(createParser(thumb_image_path))
+                            if metadata.has("width"):
+                                width = metadata.get("width")
+                            if metadata.has("height"):
+                                height = metadata.get("height")
+                        document_attributes = [
+                            DocumentAttributeVideo(
+                                duration=duration,
+                                w=width,
+                                h=height,
+                                round_message=False,
+                                supports_streaming=True
                             )
-        else:
-            await event.get_reply_message("there is no .zip link")
+                        ]
+                    try:
+                        await borg.send_file(
+                            event.chat_id,
+                            single_file,
+                            caption=f"remote unzip `{caption_rts}`",
+                            force_document=force_document,
+                            supports_streaming=supports_streaming,
+                            allow_cache=False,
+                            reply_to=event.message.id,
+                            attributes=document_attributes,
+                            # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                            #     progress(d, t, event, c_time, "trying to upload")
+                            # )
+                        )
+                    except Exception as e:
+                        await borg.send_message(
+                            event.chat_id,
+                            "{} caused `{}`".format(caption_rts, str(e)),
+                            reply_to=event.message.id
+                        )
+                        # some media were having some issues
+                        continue
+                    os.remove(single_file)
+            # os.remove(downloaded_file_name)
+            #     await borg.send_file(
+            #                     event.chat_id,
+            #                     x,
+            #                     caption="unzipped @By_Azade",
+            #                     # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+            #                     #     progress(d, t, event, c_time, "trying to upload")
+            #                     # )
+            #                 )
+            # # await event.edit(reply)
 
-    await event.edit(reply)
+
+
+def get_lst_of_files(input_directory, output_lst):
+    filesinfolder = os.listdir(input_directory)
+    for file_name in filesinfolder:
+        current_file_name = os.path.join(input_directory, file_name)
+        if os.path.isdir(current_file_name):
+            return get_lst_of_files(current_file_name, output_lst)
+        output_lst.append(current_file_name)
+    return output_lst
--- stdplugins/youtube_playlist.py
+++ stdplugins/youtube_playlist.py
@@ -1,105 +1,105 @@
-# import asyncio
-# import os
-# import shutil
-# import subprocess
-# import time
-# from pySmartDL import SmartDL
-# from sample_config import Config
-# from telethon import events
-# from uniborg.util import admin_cmd, humanbytes, progress, time_formatter
-# import subprocess
-# import patoolib
-# from hachoir.metadata import extractMetadata
-# from hachoir.parser import createParser
+import asyncio
+import os
+import shutil
+import subprocess
+import time
+from pySmartDL import SmartDL
+from sample_config import Config
+from telethon import events
+from uniborg.util import admin_cmd, humanbytes, progress, time_formatter
+import subprocess
+import patoolib
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo
+
+thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
 
 
+@borg.on(admin_cmd(pattern=("playlistd ?(.*)")))
+async def _(event):
+    if event.fwd_from:
+        return
+    url = event.pattern_match.group(1)
+    if not os.path.isdir("./DOWNLOAD/Playlist/"):
+        os.makedirs("./DOWNLOAD/Playlist/")
+    output = "./DOWNLOAD/Playlist/"
+    thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
+    mone = await event.edit("Processing ...")
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        try:
+            c_time = time.time()
+            command_to_exec = [
+                    "./bin/youtubeplaylist.py",
+                    f"{url}",
+                    f"{output}"
+                    ]
+            logger.info(command_to_exec)
+            sp = subprocess.Popen(command_to_exec, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+            filename = sorted(get_lst_of_files(output, []))
+            for single_file in filename:
+                if os.path.exists(single_file):
+                    # https://stackoverflow.com/a/678242/4723940
+                    caption_rts = os.path.basename(single_file)
+                    force_document = True
+                    supports_streaming = False
+                    document_attributes = []
+                    if single_file.endswith((".mp4", ".mp3", ".flac", ".webm")):
+                        metadata = extractMetadata(createParser(single_file))
+                        duration = 0
+                        width = 0
+                        height = 0
+                        if metadata.has("duration"):
+                            duration = metadata.get('duration').seconds
+                        if os.path.exists(thumb_image_path):
+                            metadata = extractMetadata(createParser(thumb_image_path))
+                            if metadata.has("width"):
+                                width = metadata.get("width")
+                            if metadata.has("height"):
+                                height = metadata.get("height")
+                        document_attributes = [
+                            DocumentAttributeVideo(
+                                duration=duration,
+                                w=width,
+                                h=height,
+                                round_message=False,
+                                supports_streaming=True
+                            )
+                        ]
+                    try:
+                        await borg.send_file(
+                            event.chat_id,
+                            single_file,
+                            caption=f" `{caption_rts}`",
+                            force_document=force_document,
+                            supports_streaming=supports_streaming,
+                            allow_cache=False,
+                            reply_to=event.message.id,
+                            attributes=document_attributes,
+                            # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                            #     progress(d, t, event, c_time, "trying to upload")
+                            # )
+                        )
+                    except Exception as e:
+                        await borg.send_message(
+                            event.chat_id,
+                            "{} caused `{}`".format(caption_rts, str(e)),
+                            reply_to=event.message.id
+                        )
+                        # some media were having some issues
+                        continue
+                    os.remove(single_file)
+            os.remove(output)
 
 
-
-# @borg.on(admin_cmd(pattern=("playlistd ?(.*)")))
-# async def _(event):
-#     if event.fwd_from:
-#         return
-#     url = event.pattern_match.group(1)
-#     if not os.path.isdir("./DOWNLOAD/Playlist/"):
-#         os.makedirs("./DOWNLOAD/Playlist/")
-#     output = "./DOWNLOAD/Playlist/"
-#     thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
-#     mone = await event.edit("Processing ...")
-#     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-#         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-#     if event.reply_to_msg_id:
-#         reply_message = await event.get_reply_message()
-#         try:
-#             c_time = time.time()
-#             command_to_exec = [
-#                     "./bin/youtubeplaylist.py",
-#                     f"{url}",
-#                     f"{output}"
-#                     ]
-#             logger.info(command_to_exec)
-#             sp = subprocess.Popen(command_to_exec, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-#             filename = sorted(get_lst_of_files(output, []))
-#             for single_file in filename:
-#                 if os.path.exists(single_file):
-#                     # https://stackoverflow.com/a/678242/4723940
-#                     caption_rts = os.path.basename(single_file)
-#                     force_document = True
-#                     supports_streaming = False
-#                     document_attributes = []
-#                     if single_file.endswith((".mp4", ".mp3", ".flac", ".webm")):
-#                         metadata = extractMetadata(createParser(single_file))
-#                         duration = 0
-#                         width = 0
-#                         height = 0
-#                         if metadata.has("duration"):
-#                             duration = metadata.get('duration').seconds
-#                         if os.path.exists(thumb_image_path):
-#                             metadata = extractMetadata(createParser(thumb_image_path))
-#                             if metadata.has("width"):
-#                                 width = metadata.get("width")
-#                             if metadata.has("height"):
-#                                 height = metadata.get("height")
-#                         document_attributes = [
-#                             DocumentAttributeVideo(
-#                                 duration=duration,
-#                                 w=width,
-#                                 h=height,
-#                                 round_message=False,
-#                                 supports_streaming=True
-#                             )
-#                         ]
-#                     try:
-#                         await borg.send_file(
-#                             event.chat_id,
-#                             single_file,
-#                             caption=f" `{caption_rts}`",
-#                             force_document=force_document,
-#                             supports_streaming=supports_streaming,
-#                             allow_cache=False,
-#                             reply_to=event.message.id,
-#                             attributes=document_attributes,
-#                             # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-#                             #     progress(d, t, event, c_time, "trying to upload")
-#                             # )
-#                         )
-#                     except Exception as e:
-#                         await borg.send_message(
-#                             event.chat_id,
-#                             "{} caused `{}`".format(caption_rts, str(e)),
-#                             reply_to=event.message.id
-#                         )
-#                         # some media were having some issues
-#                         continue
-#                     os.remove(single_file)
-#             os.remove(output)
-
-
-# def get_lst_of_files(input_directory, output_lst):
-#     filesinfolder = os.listdir(input_directory)
-#     for file_name in filesinfolder:
-#         current_file_name = os.path.join(input_directory, file_name)
-#         if os.path.isdir(current_file_name):
-#             return get_lst_of_files(current_file_name, output_lst)
-#         output_lst.append(current_file_name)
-#     return output_lst
+def get_lst_of_files(input_directory, output_lst):
+    filesinfolder = os.listdir(input_directory)
+    for file_name in filesinfolder:
+        current_file_name = os.path.join(input_directory, file_name)
+        if os.path.isdir(current_file_name):
+            return get_lst_of_files(current_file_name, output_lst)
+        output_lst.append(current_file_name)
+    return output_lst
