e2a22cf3622aadd09a5a1be2a380a93614a1deb9
muhammedfurkan <byazade@yandex.com>
2019-12-12 23:44:35 +0300

    update progress bar

--- stdplugins/glink.py
+++ stdplugins/glink.py
@@ -87,8 +87,8 @@ async def download(dryb):
                 speed = downloader.get_speed()
                 elapsed_time = round(diff) * 1000
                 progress_str = "[{0}{1}]\nProgress: {2}%".format(
-                    ''.join(["●" for i in range(math.floor(percentage / 5))]),
-                    ''.join(["○" for i in range(20 - math.floor(percentage / 5))]),
+                    ''.join(["█" for i in range(math.floor(percentage / 5))]),
+                    ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
                     round(percentage, 2))
                 estimated_total_time = downloader.get_eta(human=True)
                 try:
