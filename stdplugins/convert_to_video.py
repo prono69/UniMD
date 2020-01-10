cb6dc20e525c6dda910bb9738764459156c445c6
M.Furkan <byazade@yandex.com>
2020-01-10 21:04:12 +0300

    .

--- stdplugins/convert_to_video.py
+++ stdplugins/convert_to_video.py
@@ -29,7 +29,7 @@ async def _(event):
     mone = await event.edit("Processing ...")
     input_str = event.pattern_match.group(1)
     thumb = None
-    thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + str(time.time()) + ".jpg"
+    thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "thumb_image.jpg"
     logger.info(thumb_image_path)
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
