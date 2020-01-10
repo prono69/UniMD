ac6bdca7bfd8cf2984cb358e1cb56bb08ea94386
M.Furkan <byazade@yandex.com>
2020-01-10 16:19:59 +0300

    .

--- stdplugins/convert_to_video.py
+++ stdplugins/convert_to_video.py
@@ -27,8 +27,12 @@ async def _(event):
     if event.fwd_from:
         return
     mone = await event.edit("Processing ...")
+    input_str = event.pattern_match.group(1)
+    thumb = None
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    if os.path.exists(thumb_image_path):
+        thumb = thumb_image_path
     if event.reply_to_msg_id:
         start = datetime.now()
         reply_message = await event.get_reply_message()
@@ -46,188 +50,113 @@ async def _(event):
         else:
             end = datetime.now()
             ms = (end - start).seconds
-            await mone.edit("Stored the rar to `{}` in {} seconds.".format(downloaded_file_name, ms))
+            await mone.edit("Downloaded now preparing to streaming upload")
+        # if os.path.exists(input_str):
+            await borg.send_file(
+                event.chat_id,
+                downloaded_file_name,
+                caption=caption_rts,
+                force_document=force_document,
+                supports_streaming=True,
+                allow_cache=False,
+                reply_to=event.message.id,
+                thumb=thumb,
+                attributes=document_attributes,
+            )
 
-        # patoolib.extract_archive(downloaded_file_name, outdir=extracted)
-        filename = sorted(get_lst_of_files(Config.TMP_DOWNLOAD_DIRECTORY, []))
-        #filename = filename + "/"
-        await event.edit("Unraring now")
-        # r=root, d=directories, f = files
-        for single_file in filename:
-            if os.path.exists(single_file):
-                # https://stackoverflow.com/a/678242/4723940
-                caption_rts = os.path.basename(single_file)
-                force_document = False
-                supports_streaming = True
-                document_attributes = []
-                if single_file.upper().endswith(Config.TL_VID_STREAM_TYPES):
-                    metadata = extractMetadata(createParser(single_file))
-                    duration = 0
-                    width = 0
-                    height = 0
-                    if metadata.has("duration"):
-                        duration = metadata.get('duration').seconds
-                    # if os.path.exists(thumb_image_path):
-                    #     metadata = extractMetadata(createParser(thumb_image_path))
-                    #     if metadata.has("width"):
-                    #         width = metadata.get("width")
-                    #     if metadata.has("height"):
-                    #         height = metadata.get("height")
-                    document_attributes = [
-                        DocumentAttributeVideo(
-                            duration=duration,
-                            w=width,
-                            h=height,
-                            round_message=False,
-                            supports_streaming=True
-                        )
-                    ]
-                try:
-                    await borg.send_file(
-                        event.chat_id,
-                        single_file,
-                        caption=f"UnRarred `{caption_rts}`",
-                        force_document=force_document,
-                        supports_streaming=supports_streaming,
-                        allow_cache=False,
-                        reply_to=event.message.id,
-                        attributes=document_attributes,
-                        # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                        #     progress(d, t, event, c_time, "trying to upload")
-                        # )
-                    )
-                except Exception as e:
-                    await borg.send_message(
-                        event.chat_id,
-                        "{} caused `{}`".format(caption_rts, str(e)),
-                        reply_to=event.message.id
-                    )
-                    # some media were having some issues
-                    continue
-                # os.remove(single_file)
-        # os.remove(downloaded_file_name)
-# async def _(event):
-#     if event.fwd_from:
-#         return
-#     mone = await event.edit("Processing ...")
-#     input_str = event.pattern_match.group(1)
-#     thumb = None
-#     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-#         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-#     if os.path.exists(thumb_image_path):
-#         thumb = thumb_image_path
-#     if event.reply_to_msg_id:
-#         start = datetime.now()
-#         reply_message = await event.get_reply_message()
-#         try:
-#             c_time = time.time()
-#             downloaded_file_name = await borg.download_media(
-#                 reply_message,
-#                 Config.TMP_DOWNLOAD_DIRECTORY,
-#                 progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-#                     progress(d, t, mone, c_time, "trying to download")
-#                 )
-#             )
-#         except Exception as e:  # pylint:disable=C0103,W0703
-#             await mone.edit(str(e))
-#         else:
-#             end = datetime.now()
-#             ms = (end - start).seconds
-#             await mone.edit("Downloaded now preparing to streaming upload")
-#         # if os.path.exists(input_str):
-#             start = datetime.now()
-#             lst_of_files = sorted(get_lst_of_files(Config.TMP_DOWNLOAD_DIRECTORY, []))
-#             logger.info(lst_of_files)
-#             u = 0
-#             await event.edit(
-#                 "Found {} files. ".format(len(lst_of_files)) + \
-#                 "Uploading will start soon. " + \
-#                 "Please wait!"
-#             )
-#             thumb = None
-#             if os.path.exists(thumb_image_path):
-#                 thumb = thumb_image_path
-#             for single_file in lst_of_files:
-#                 if os.path.exists(single_file):
-#                     caption_rts = os.path.basename(single_file)
-#                     force_document = True
-#                     supports_streaming = False
-#                     document_attributes = []
-#                     width = 0
-#                     height = 0
-#                     if os.path.exists(thumb_image_path):
-#                         metadata = extractMetadata(createParser(thumb_image_path))
-#                         if metadata.has("width"):
-#                             width = metadata.get("width")
-#                         if metadata.has("height"):
-#                             height = metadata.get("height")
-#                     if single_file.upper().endswith(Config.TL_VID_STREAM_TYPES):
-#                         metadata = extractMetadata(createParser(single_file))
-#                         duration = 0
-#                         if metadata.has("duration"):
-#                             duration = metadata.get('duration').seconds
-#                         document_attributes = [
-#                             documentAttributeVideo(
-#                                 duration=duration,
-#                                 w=width,  
-#                                 h=height,
-#                                 round_message=False,
-#                                 supports_streaming=True
-#                             )
-#                         ]
-#                         supports_streaming = True
-#                         force_document = False
-#                     if single_file.upper().endswith(Config.TL_MUS_STREAM_TYPES):
-#                         metadata = extractMetadata(createParser(single_file))
-#                         duration = 0
-#                         title = ""
-#                         artist = ""
-#                         if metadata.has("duration"):
-#                             duration = metadata.get('duration').seconds
-#                         if metadata.has("title"):
-#                             title = metadata.get("title")
-#                         if metadata.has("artist"):
-#                             artist = metadata.get("artist")
-#                         document_attributes = [
-#                             DocumentAttributeAudio(
-#                                 duration=duration,
-#                                 voice=False,
-#                                 title=title,
-#                                 performer=artist,
-#                                 waveform=None
-#                             )
-#                         ]
-#                         supports_streaming = True
-#                         force_document = False
-#                     try:
-#                         await borg.send_file(
-#                             event.chat_id,
-#                             single_file,
-#                             caption=caption_rts,
-#                             force_document=force_document,
-#                             supports_streaming=supports_streaming,
-#                             allow_cache=False,
-#                             reply_to=event.message.id,
-#                             thumb=thumb,
-#                             attributes=document_attributes,
-#                         )
-#                     except Exception as e:
-#                         await borg.send_message(
-#                             event.chat_id,
-#                             "{} caused `{}`".format(caption_rts, str(e)),
-#                             reply_to=event.message.id
-#                         )
-#                         continue
-#                     os.remove(single_file)
-#                     u = u + 1
-#                     await asyncio.sleep(5)
-#             end = datetime.now()
-#             ms = (end - start).seconds
-#             await event.edit("Uploaded {} files in {} seconds.".format(u, ms))
-#             await asyncio.sleep(5)
-#             os.remove(downloaded_file_name)
-#     else:
-#         await event.edit("404: Directory Not Found")
+    #         start = datetime.now()
+    #         lst_of_files = sorted(get_lst_of_files(Config.TMP_DOWNLOAD_DIRECTORY, []))
+    #         logger.info(lst_of_files)
+    #         u = 0
+    #         await event.edit(
+    #             "Found {} files. ".format(len(lst_of_files)) + \
+    #             "Uploading will start soon. " + \
+    #             "Please wait!"
+    #         )
+    #         thumb = None
+    #         if os.path.exists(thumb_image_path):
+    #             thumb = thumb_image_path
+    #         for single_file in lst_of_files:
+    #             if os.path.exists(single_file):
+    #                 caption_rts = os.path.basename(single_file)
+    #                 force_document = True
+    #                 supports_streaming = False
+    #                 document_attributes = []
+    #                 width = 0
+    #                 height = 0
+    #                 if os.path.exists(thumb_image_path):
+    #                     metadata = extractMetadata(createParser(thumb_image_path))
+    #                     if metadata.has("width"):
+    #                         width = metadata.get("width")
+    #                     if metadata.has("height"):
+    #                         height = metadata.get("height")
+    #                 if single_file.upper().endswith(Config.TL_VID_STREAM_TYPES):
+    #                     metadata = extractMetadata(createParser(single_file))
+    #                     duration = 0
+    #                     if metadata.has("duration"):
+    #                         duration = metadata.get('duration').seconds
+    #                     document_attributes = [
+    #                         documentAttributeVideo(
+    #                             duration=duration,
+    #                             w=width,  
+    #                             h=height,
+    #                             round_message=False,
+    #                             supports_streaming=True
+    #                         )
+    #                     ]
+    #                     supports_streaming = True
+    #                     force_document = False
+    #                 if single_file.upper().endswith(Config.TL_MUS_STREAM_TYPES):
+    #                     metadata = extractMetadata(createParser(single_file))
+    #                     duration = 0
+    #                     title = ""
+    #                     artist = ""
+    #                     if metadata.has("duration"):
+    #                         duration = metadata.get('duration').seconds
+    #                     if metadata.has("title"):
+    #                         title = metadata.get("title")
+    #                     if metadata.has("artist"):
+    #                         artist = metadata.get("artist")
+    #                     document_attributes = [
+    #                         DocumentAttributeAudio(
+    #                             duration=duration,
+    #                             voice=False,
+    #                             title=title,
+    #                             performer=artist,
+    #                             waveform=None
+    #                         )
+    #                     ]
+    #                     supports_streaming = True
+    #                     force_document = False
+    #                 try:
+    #                     await borg.send_file(
+    #                         event.chat_id,
+    #                         single_file,
+    #                         caption=caption_rts,
+    #                         force_document=force_document,
+    #                         supports_streaming=supports_streaming,
+    #                         allow_cache=False,
+    #                         reply_to=event.message.id,
+    #                         thumb=thumb,
+    #                         attributes=document_attributes,
+    #                     )
+    #                 except Exception as e:
+    #                     await borg.send_message(
+    #                         event.chat_id,
+    #                         "{} caused `{}`".format(caption_rts, str(e)),
+    #                         reply_to=event.message.id
+    #                     )
+    #                     continue
+    #                 os.remove(single_file)
+    #                 u = u + 1
+    #                 await asyncio.sleep(5)
+    #         end = datetime.now()
+    #         ms = (end - start).seconds
+    #         await event.edit("Uploaded {} files in {} seconds.".format(u, ms))
+    #         await asyncio.sleep(5)
+    #         os.remove(downloaded_file_name)
+    # else:
+    #     await event.edit("404: Directory Not Found")
 
 def get_lst_of_files(input_directory, output_lst):
     filesinfolder = os.listdir(input_directory)
