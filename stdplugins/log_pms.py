2614cf0eeca6a75aa6850ab8092bb0f785368fad
M.Furkan <byazade@yandex.com>
2020-01-11 15:44:28 +0300

    update

--- stdplugins/log_pms.py
+++ stdplugins/log_pms.py
@@ -4,7 +4,7 @@ import asyncio
 from telethon import events
 from telethon.tl import functions, types
 from uniborg.util import admin_cmd
-
+import sys
 from sample_config import Config
 
 global NO_PM_LOG_USERS
@@ -26,6 +26,10 @@ async def monito_p_m_s(event):
                 )
             except Exception as e:
                 logger.warn(str(e))
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print(exc_type, fname, exc_tb.tb_lineno)
+                print(e) 
 
 
 @borg.on(events.NewMessage(pattern="nolog ?(.*)"))
