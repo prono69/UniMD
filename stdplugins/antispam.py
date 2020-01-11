f566abec60c426cadbb00f8e87960292ee704421
M.Furkan <byazade@yandex.com>
2020-01-11 15:53:42 +0300

    telethon==1.10.9 and update antispambot

--- stdplugins/antispam.py
+++ stdplugins/antispam.py
@@ -1,6 +1,7 @@
 #COMBOT ANTI SPAM SYSTEM IS USED
 #created for @uniborg (unfinished)
 
+from requests import get
 from telethon import events
 from telethon.events import ChatAction
 from telethon.tl.types import ChannelParticipantsAdmins, Message
@@ -16,24 +17,23 @@ async def _(cas):
         if cas.user_joined or cas.user_added: 
             user = await cas.get_user()
             id = user.id
-        mid = "{}".format(chat.title)
-        mention = "[{}](tg://user?id={})".format(user.first_name, user.id) 
-        from requests import get
-        r = get(f'https://combot.org/api/cas/check?user_id={id}') 
-        r_dict = r.json() 
-        if r_dict['ok']:
-            try:                
-                more = r_dict['result']
-                from telethon.tl.types import ChatBannedRights
-                from telethon.tl.functions.channels import EditBannedRequest
-                rights = ChatBannedRights(
-                    until_date=None,
-                    view_messages=True,
-                    send_messages=True
-                )
-                await borg(EditBannedRequest(cas.chat_id, id, rights))
-                await borg.send_message(Config.PRIVATE_GROUP_BOT_API_ID, "**antispam log** \n**Who**: {} \n**Where**: {} \n**How**: [here](https://combot.org/api/cas/check?user_id={}) \n**Action**: Banned \n**More**: ```{}```".format(mention, mid, id, more),link_preview=True)
-            except (Exception) as exc:
-                await borg.send_message(Config.PRIVATE_GROUP_BOT_API_ID, str(exc))
+            mid = "{}".format(chat.title)
+            mention = "[{}](tg://user?id={})".format(user.first_name, user.id) 
+            r = get(f'https://combot.org/api/cas/check?user_id={id}') 
+            r_dict = r.json() 
+            if r_dict['ok']:
+                try:                
+                    more = r_dict['result']
+                    from telethon.tl.types import ChatBannedRights
+                    from telethon.tl.functions.channels import EditBannedRequest
+                    rights = ChatBannedRights(
+                        until_date=None,
+                        view_messages=True,
+                        send_messages=True
+                    )
+                    await borg(EditBannedRequest(cas.chat_id, id, rights))
+                    await borg.send_message(Config.PRIVATE_GROUP_BOT_API_ID, "**antispam log** \n**Who**: {} \n**Where**: {} \n**How**: [here](https://combot.org/api/cas/check?user_id={}) \n**Action**: Banned \n**More**: ```{}```".format(mention, mid, id, more),link_preview=True)
+                except (Exception) as exc:
+                    await borg.send_message(Config.PRIVATE_GROUP_BOT_API_ID, str(exc))
     else:
         return ""
