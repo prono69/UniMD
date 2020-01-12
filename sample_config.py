14996ce7a2f43151d7279327b585713eb5b81d4a
M.Furkan <byazade@yandex.com>
2020-01-12 12:02:35 +0300

    just testing other antispam plugin

--- sample_config.py
+++ sample_config.py
@@ -60,7 +60,8 @@ class Config(object):
     # because of their ephemeral file system
     NO_LOAD = [
         "gban",
-        "notification_mtab_manager"
+        "notification_mtab_manager",
+        "antispam"
     ]
     # Get your own API key from https://www.remove.bg/ or
     # feel free to use http://telegram.dog/Remove_BGBot
